2c9677ebb755fbdfc1fb25137bf1a597f8f7e6b4
Kleber Wagner Junior <kleberwagner107@gmail.com>
2025-04-15 09:10:46 -0300

    update

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,10 +1,9 @@
 i = 0
 soma = 0
-quant = int(input("Digite quantos números vocÊ quer usar para tirar a média: "))
-while i < quant:
+while i < 10:
     somatotal=float(input(f"Digite o {i+1}° valor: "))
     soma+=somatotal
     i+=1
-media = soma/quant
+media = soma/10
 print(" ")
 print(media)
